ee1c6ffa4d4f2ba63a52ea0b88f4f2ad9889de55
BiancaDullius <bianca.dullius@paipe.co>
2022-11-21 18:56:41 -0300

    ajuste variaveis

--- main.c
+++ main.c
@@ -2,18 +2,15 @@
 
 int main()
 {
-    int countNums = 0, countSequence = 0, result;
-    int n = 9;
-    int nums[9] = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+    int n = 3, result = 0;
+    int nums[3] = {0, 3, 1};
 
     for (int i = 0; i < n; i++)
     {
-        countNums += nums[i];
-        countSequence += i;
+        result = result + i - nums[i];
     }
-    result = (countSequence + n) - countNums;
 
-    printf("%d", result);
+    printf("%d", result + n);
 
     return 0;
 }
